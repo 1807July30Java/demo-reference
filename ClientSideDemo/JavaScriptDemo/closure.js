9f728859073587e366025cb8cc883b1d742bba3a
Emily Higgins <emilyrchiggins@gmail.com>
2018-08-16 12:04:25 -0400

    parsing

--- ClientSideDemo/JavaScriptDemo/closure.js
+++ ClientSideDemo/JavaScriptDemo/closure.js
@@ -1,19 +1,40 @@
 //behavior that will execute when page loads
-window.onload = function() {
+window.onload = function () {
     console.log("page loaded!");
     counter = getCount();
     bearCounter = getCount("bear");
     penguinCounter = getCount("penguin");
+    failedLoginCounter = getCount("failed login");
+    isLoggedIn = false;
 }
 
-function getCount(noun){
+function getCount(noun) {
     var count = 0;
-    return function(){
-        count +=1;
+    return function () {
+        count += 1;
         if (noun) {
-            console.log(noun+" count is: "+count);
+            console.log(noun + " count is: " + count);
         } else {
-            console.log("count is: "+count);
+            console.log("count is: " + count);
+        }
+        return count;
+    }
+}
+
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username && password ){
+        if (username === "Aretha" && password === 'awesome'){
+            isLoggedIn = true;
+            console.log("success");
+            //do some logic
+        } else {
+            isLoggedIn = false;
+            if (failedLoginCounter() > 3) {
+                console.log("too many attempts! Account locked");
+                //do some logic
+            }
         }
     }
 }

c74705abbf9e6198d3b94e870338804dfda11a12
Emily Higgins <emilyrchiggins@gmail.com>
2018-08-24 13:47:14 -0400

    finishing databinding, for real

--- angular-demo/src/app/components/databinding/databinding.component.ts
+++ angular-demo/src/app/components/databinding/databinding.component.ts
@@ -15,6 +15,45 @@ export class DatabindingComponent implements OnInit {
   interpolationNotation = '{{}}';
   interpolation = 'Welcome to the Interpolation.';
 
+  // For Event Binding
+ eventBindingNotation = '()';
+
+ public counter = 0;
+
+ public incrementCounter(): void {
+   this.counter++;
+ }
+
+ // For Two-Way Data Binding
+ twoWayBindingNotation = '[()]';
+
+ public user = {
+   email: '',
+   password: ''
+ };
+
+// For Property Binding
+propertyBindingNotation = '[]';
+
+public objectStyle = {
+  color: 'red',
+  border: '1px solid black',
+  cursor: 'pointer',
+  margin: '2px'
+};
+
+public changeStyles(): void {
+  if (this.objectStyle.color === 'red') {
+    this.objectStyle.color = 'blue';
+    this.objectStyle.border = '4px groove purple';
+  } else if (this.objectStyle.color === 'blue') {
+    this.objectStyle.color = 'green';
+    this.objectStyle.border = '4px ridge yellow';
+  } else {
+    this.objectStyle.color = 'red';
+    this.objectStyle.border = '4px solid black';
+  }
+}
 
  ngOnInit() {
 

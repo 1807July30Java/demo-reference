c67f5b521aac18d6eebaa3387e38f3c5085ad41f
Emily Higgins <emilyrchiggins@gmail.com>
2018-08-29 12:31:09 -0400

    HibernateDemo structure

---
+++ HibernateDemo/src/main/java/com/revature/main/Driver.java
@@ -0,0 +1,21 @@
+package com.revature.main;
+
+import org.hibernate.Session;
+
+import com.revature.util.HibernateUtil;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+
+		//get a session!
+		
+		Session s = HibernateUtil.getSession();
+		
+		System.out.println(s.getStatistics());
+		
+		s.close();
+
+	}
+
+}
